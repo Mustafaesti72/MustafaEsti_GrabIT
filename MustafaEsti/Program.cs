23ca3ca833e5424518db9935722b31b17b13210c
Mustafaesti72 <mustafaesti@marun.edu.tr>
2022-12-30 18:36:03 +0100

    User services added.

--- MustafaEsti/Program.cs
+++ MustafaEsti/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using MustafaEsti.Data;
+using WebApi.Authorization;
+using WebApi.Helpers;
+using WebApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,16 +16,42 @@ builder.Services.AddDbContext<DataContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddAutoMapper(typeof(Program));
+//builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+builder.Services.AddScoped<IJwtUtils, JwtUtils>();
+builder.Services.AddScoped<IUserService, UserService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+//app.UseSwagger();
+//app.UseSwaggerUI();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MustafaEsti v1"));
 }
-
+app.UseSwagger(c =>
+{
+    c.PreSerializeFilters.Add((document, request) =>
+    {
+        var paths = document.Paths.ToDictionary(i => i.Key.ToLowerInvariant(), i => i.Value);
+        document.Paths.Clear();
+        foreach (var pathItem in paths)
+        {
+            document.Paths.Add(pathItem.Key, pathItem.Value);
+        }
+    });
+});
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "MustafaEsti API v1");
+});
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
